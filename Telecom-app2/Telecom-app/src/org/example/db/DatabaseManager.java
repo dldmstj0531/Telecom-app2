3f5d3fbc41d313e7c62ac0e1ebe2d7cb96e758db
lee seo yool <128380187+dldmstj0531@users.noreply.github.com>
2025-07-06 19:34:26 +0900

    Update DatabaseManager.java
    DB 비밀번호 수정

--- Telecom-app2/Telecom-app/src/org/example/db/DatabaseManager.java
+++ Telecom-app2/Telecom-app/src/org/example/db/DatabaseManager.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/telecom";
     private static final String USER = "root";
-    private static final String PASSWORD = "changemanage1234"; // 각자 설정한 비밀번호로 입력
+    private static final String PASSWORD = ""; // 각자 설정한 비밀번호로 입력
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
